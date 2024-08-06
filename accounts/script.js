98a8c74818bef9d9508f563fcd3e4efb3073154e
itcglobalcap <info.icontradecapital@gmail.com>
2024-08-06 12:22:58 +0100

    Delete accounts directory

--- accounts/script.js
+++
@@ -1,5 +0,0 @@
-const ranger = document.querySelector('#speed');
-const speedUpdate = () => {
-  document.documentElement.style.setProperty('--speed', `${ranger.value}s`);
-};
-ranger.addEventListener('input', speedUpdate);
